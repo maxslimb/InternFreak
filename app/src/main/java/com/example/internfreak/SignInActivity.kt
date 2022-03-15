a5138ccc9a9fe3d4e69f99d0dab576c4f7f7f87d
Karan <karan.savaliya01@gmail.com>
2022-03-15 23:04:53 +0530

    Implemented Firebase

--- app/src/main/java/com/example/internfreak/SignInActivity.kt
+++ app/src/main/java/com/example/internfreak/SignInActivity.kt
@@ -1,13 +1,23 @@
 package com.example.internfreak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        val google_signin_btn =findViewById<com.google.android.gms.common.SignInButton>(R.id.google_sign_in)
 
+        google_signin_btn.setOnClickListener {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
