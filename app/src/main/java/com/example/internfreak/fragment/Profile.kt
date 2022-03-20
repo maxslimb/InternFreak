e014faf60070374935880fcd3b960bbf0b93b01b
maxslimb <kpatel3032@gmail.com>
2022-03-20 17:25:21 +0530

    added intent for edit profile

--- app/src/main/java/com/example/internfreak/fragment/Profile.kt
+++ app/src/main/java/com/example/internfreak/fragment/Profile.kt
@@ -1,28 +1,30 @@
 package com.example.internfreak.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.internfreak.EditProfile
 import com.example.internfreak.R
 
 
 class Profile : Fragment() {
 
-/*
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding= FragmentProfileBinding.inflate(layoutInflater)
-        binding.btn_edit_profile.setOnClickListener{
-            val intent=Intent(this@Profile.requireContext(),EditProfile::class.java)
+        val btn_rdit_profile= view.findViewById<Button>(R.id.btn_edit_profile)
+        btn_rdit_profile.setOnClickListener{
+            val intent= Intent(view.context, EditProfile::class.java)
             startActivity(intent)
-
         }
 
 
-    }*/
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
